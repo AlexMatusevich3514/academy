0b4bffbc7e599693ad2e15255c3b2502e89bc6be
AlexMatusevich3514 <matusevich3514@gmail.com>
2022-12-01 20:28:15 +0300

    homevorkTask1

---
+++ src/lesson4/classwork/ArrayTask1.java
@@ -0,0 +1,24 @@
+package lesson4.classwork;
+
+import java.util.Arrays;
+
+public class ArrayTask1 {
+    public static void main(String[] args) {
+
+        int j = 0;
+        int length = 0;
+        for (int i = 2; i < 20; i++) {
+            if (i % 2 == 0) {
+                length++;
+            }
+        }
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            if (i >= 2 && i < 20 && i % 2 == 0) {
+                array[j] = i;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
